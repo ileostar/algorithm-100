af9e1e1333dc34d8c3057886abd8b4b80c544285
leostar <hi@leostar.top>
2024-06-20 14:56:59 +0800

    chore: update plop

--- plopfile.js
+++ plopfile.js
@@ -1,12 +1,37 @@
-export default function (plop) {
+import fs from 'node:fs'
+
+/**
+ * 获取src下第一层文件夹数量
+ */
+function getCurrentFileNum(dirPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dirPath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        reject(err)
+      }
+      else {
+        let dirCount = 0
+        for (const file of files) {
+          if (file.isDirectory())
+            dirCount++
+        }
+        resolve(dirCount)
+      }
+    })
+  })
+}
+
+// 使用示例
+export default async function (plop) {
+  let number = await getCurrentFileNum('./src')
+  if (number < 10)
+    number = `00${number + 1}`
+  else if (number >= 10 && number < 100)
+    number = `0${number + 1}`
+
   plop.setGenerator('basicAlgo', {
     description: '创建你的基础vue3模板文件',
     prompts: [
-      {
-        type: 'input',
-        name: 'number',
-        message: '请输入你算法序号',
-      },
       {
         type: 'input',
         name: 'name',
@@ -15,15 +40,15 @@ export default function (plop) {
     ],
     actions: [{
       type: 'add',
-      path: 'src/{{number}}-{{dashCase name}}/index.ts',
+      path: `src/${number}-{{dashCase name}}/index.ts`,
       templateFile: 'plop-template/index.hbs',
     }, {
       type: 'add',
-      path: 'src/{{number}}-{{dashCase name}}/index.spec.ts',
+      path: `src/${number}-{{dashCase name}}/index.spec.ts`,
       templateFile: 'plop-template/index.spec.hbs',
     }, {
       type: 'add',
-      path: 'src/{{number}}-{{dashCase name}}/readme.md',
+      path: `src/${number}-{{dashCase name}}/readme.md`,
       templateFile: 'plop-template/readme.hbs',
     }],
   })
