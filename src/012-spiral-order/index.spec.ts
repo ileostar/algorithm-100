1ea92024c63ea4d94e5456cd726425f80fc19dee
ileostar <hi@leostar.top>
2024-06-26 22:12:29 +0800

    🔥 remove lint warning

--- src/012-spiral-order/index.spec.ts
+++ src/012-spiral-order/index.spec.ts
@@ -1,15 +1,15 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest'
 import { spiralOrder } from './index'
 
-describe('spiralOrder',()=>{
-  it('case one',()=>{
-    const matrix = [[1,2,3],[4,5,6],[7,8,9]]
+describe('spiralOrder', () => {
+  it('case one', () => {
+    const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     const res = spiralOrder(matrix)
-    expect(res).toEqual([1,2,3,6,9,8,7,4,5])
+    expect(res).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5])
   })
-  it('case two',()=>{
-    const matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+  it('case two', () => {
+    const matrix = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
     const res = spiralOrder(matrix)
-    expect(res).toEqual([1,2,3,4,8,12,11,10,9,5,6,7])
+    expect(res).toEqual([1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7])
   })
 })
--- src/012-spiral-order/index.ts
+++ src/012-spiral-order/index.ts
@@ -3,32 +3,36 @@
  * https://leetcode.cn/problems/spiral-matrix/submissions/542015729/?envType=study-plan-v2&envId=2024-spring-sprint-100
  */
 export function spiralOrder(matrix: number[][]): number[] {
-  const res: number[] = [];
-  if (matrix.length === 0) return res;
+  const res: number[] = []
+  if (matrix.length === 0)
+    return res
 
-  let left = 0,
-      top = 0,
-      right = matrix[0].length - 1,
-      bottom = matrix.length - 1;
+  let left = 0
+  let top = 0
+  let right = matrix[0].length - 1
+  let bottom = matrix.length - 1
 
   while (true) {
-      // 从左向右打印，上边界向下收缩一层
-      for (let i = left; i <= right; i++) res.push(matrix[top][i]);
-      if (++top > bottom) break;
+    // 从左向右打印，上边界向下收缩一层
+    for (let i = left; i <= right; i++) res.push(matrix[top][i])
+    if (++top > bottom)
+      break
 
-      // 向上到下打印，右边界向左收缩一层
-      for (let i = top; i <= bottom; i++) res.push(matrix[i][right]);
-      if (left > --right) break;
+    // 向上到下打印，右边界向左收缩一层
+    for (let i = top; i <= bottom; i++) res.push(matrix[i][right])
+    if (left > --right)
+      break
 
-      // 从右向左打印，下边界向上收缩一层
-      for (let i = right; i >= left; i--) res.push(matrix[bottom][i]);
-      if (top > --bottom) break;
+    // 从右向左打印，下边界向上收缩一层
+    for (let i = right; i >= left; i--) res.push(matrix[bottom][i])
+    if (top > --bottom)
+      break
 
-      // 从下向上打印，左边界向右收缩一层
-      for (let i = bottom; i >= top; i--) res.push(matrix[i][left]);
-      if (++left > right) break;
+    // 从下向上打印，左边界向右收缩一层
+    for (let i = bottom; i >= top; i--) res.push(matrix[i][left])
+    if (++left > right)
+      break
   }
 
-  return res;
+  return res
 };
-
