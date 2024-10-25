0a9a4391cda7fe7870c3c421a4c8e3c5fc1019bc
ileostar <hi@leostar.top>
2024-10-25 17:30:10 +0800

    🚨 remove lint warning

--- src/014-array-rotate/index.spec.ts
+++ src/014-array-rotate/index.spec.ts
@@ -1,15 +1,15 @@
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it } from 'vitest'
 import { arrayRotate } from './index'
 
-describe('arrayRotate',()=>{
-  it('case one',()=>{
-    const matrix = [[1,2,3],[4,5,6],[7,8,9]]
+describe('arrayRotate', () => {
+  it('case one', () => {
+    const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     const res = arrayRotate(matrix)
     expect(JSON.stringify(res)).toEqual('[[7,4,1],[8,5,2],[9,6,3]]')
   })
 
-  it('case two',()=>{
-    const matrix =  [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+  it('case two', () => {
+    const matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
     const res = arrayRotate(matrix)
     expect(JSON.stringify(res)).toEqual('[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]')
   })
